d10b9485f52319817e914f1c4a181d878871e362
Antilef <fi.antilef@gmail.com>
2024-11-19 15:36:31 -0300

    Add basic product selector

--- src/components/ProductSelector.jsx
+++ src/components/ProductSelector.jsx
@@ -1,12 +1,18 @@
 export default function ProductSelector(){
     return (
-        <form action="max-w-sm mx-auto">
-            <label htmlFor="products">Selecciona tu producto</label>
-            <select itemID="products">
-                <option value="MAX XL"></option>
-                <option value="PRO MAX"></option>
-                <option value="GRAND INTER"></option>
-            </select>
-        </form>
+        <div className="w-[400px] min-w-[200px] mt-4" >
+            <div className="relative">
+                <form action="" className="">
+                    <label htmlFor="products" className="pr-8">Selecciona tu producto</label>
+                    <select className="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm rounded pl-3 pr-8 py-2" itemID="products">
+                        <option value="MAX XL">Plan l max</option>
+                        <option value="PRO MAX">PLAN MAX RRSS</option>
+                        <option value="GRAND INTER">Plan XL MAX LIBRE</option>
+                    </select>
+                </form>
+            </div>
+            
+        </div>
+        
     )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,19 @@
 import NavBar from "../components/NavBar";
+import ProductSelector from "../components/ProductSelector";
 
 export default function HomePage(){
     return (
-        <NavBar/>
+        <>
+            <NavBar/>
+
+            <ProductSelector/>
+
+            <div className="grid-cols-2">
+                <div>
+                </div>
+                <div>
+                </div>
+            </div>
+        </>
     )
 }
